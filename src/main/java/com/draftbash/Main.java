17378980d8b4af140fe44adaad54c1fdfd6f34d8
StephenFeddes <feddessteve@gmail.com>
2024-05-13 03:04:33 -0500

    Testing CICD

--- src/main/java/com/draftbash/Main.java
+++ src/main/java/com/draftbash/Main.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-// Main class to run the Spring Boot application
 @SpringBootApplication
 @RestController
 public class Main {
